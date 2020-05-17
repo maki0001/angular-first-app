5556ee6ff3134f42925a9d90b96bedb8fefd3f7a
mkch <makiuchi3@gmail.com>
2020-05-17 13:30:15 +0900

    firebase関連ファイル追加

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: "app-product-alerts",
-  templateUrl: "./product-alerts.component.html",
-  styleUrls: ["./product-alerts.component.css"]
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
   @Output() notify = new EventEmitter();
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 }
